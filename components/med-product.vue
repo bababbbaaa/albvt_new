aa3df309dd795e5189f449ece6ee63f7cd788aa0
foxsis <va.four2016@gmail.com>
2023-01-18 20:25:31 +0300

    fix mob med-pdoduct

--- components/med-product.vue
+++ components/med-product.vue
@@ -106,7 +106,7 @@
     </div>
     <div
       v-else-if="item.attributes.Active == false || getConplectsActive == 1"
-      class="w-full flex items-center justify-center gap-1"
+      class="col-span-4 lg:col-span-2 xl:col-span-1  w-full flex items-center justify-center py-2 gap-2"
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -122,7 +122,7 @@
           d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
         />
       </svg>
-      <span class=" text-center text-danger text-sm">Временно недоступен</span>
+      <span class=" text-start text-danger text-xs">Временно недоступен</span>
     </div>
     <button
       @click="addToCart(item)"
